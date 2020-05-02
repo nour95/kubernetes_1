e205506d21ad382df491f3e98ba45d04b6d0bed3
almajni <almajni@kth.se>
2020-05-02 04:10:05 +0200

    test1

---
+++ commands.md
@@ -0,0 +1,45 @@
+- starting minikube:
+minikube start --driver=virtualbox
+
+- stop minikube:
+minikube stop
+
+- to clean up:
+minikube delete
+
+
+- To get the External Ip address as if you are on the cloud
+minikube service node-service-example
+
+- To build inside
+docker build -t j1:latest -f deploy/Dockerfile2 .
+
+
+- To push to docker hub
+docker login
+docker tag j1:latest nouralhuda95/j1:latest
+docker push nouralhuda95/j1:latest
+
+- To run ansible playbook
+ansible-playbook playbook.yml --extra-vars "image_id=nouralhuda95/j1"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+------------------------
---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var requests=0;
+var podname= process.env.HOSTNAME;
+var host;
+var versionNumber = 2;
+
+var handleRequest = function(request, response)
+{
+  response.setHeader('Content-Type', 'text/plain');
+  response.writeHead(200);
+  response.write("Current host is: " + host + "\n");
+  if (requests == 10 ) {
+    response.write("That is boring. you requested the time many times. Go and do something else.");
+  } else {
+    response.write("Hello! The time now is: " + new Date());
+    requests++;
+  }
+  response.end("\nVersion=" + versionNumber);
+  console.log("Current host is:" ,host);
+}
+
+var www = http.createServer(handleRequest);
+www.listen(8080,function ()
+{
+    host = process.env.HOSTNAME;
+    console.log ( "Current host is: " ,host, ", the pod name is:", podname );
+});
